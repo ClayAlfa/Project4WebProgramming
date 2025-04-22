a151a1e1cb45cc96d85b186fca8c0fc4ddc611ec
ClayAlfa <alfarabibravo8@gmail.com>
2025-04-22 10:42:04 +0800

    First Upload

---
+++ PHPComments.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+// This is a single-line comment
+echo "Ini adalah Contoh Single Line";
+echo "Ini adalah Contoh Single Line"; # This is also a single-line comment
+/* This is a
+multi-line comment */
+echo "Ini adalah Contoh Multi Line";
+?>
+ 
+</body>
+</html>
